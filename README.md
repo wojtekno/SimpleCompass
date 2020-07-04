7c1e00fe8a14a395d34014d8330ba3572c26e664
Wojtek <wjw.nowak@gmail.com>
2020-07-04 08:29:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple_Compass app
+Wojtek Nowak
+Recruitment task
---
+++ app/src/main/java/com/nowak/wjw/simplecompass/MyApplication.java
@@ -0,0 +1,8 @@
+package com.nowak.wjw.simplecompass;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+
+}
